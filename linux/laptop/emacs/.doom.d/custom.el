79084532023ca126805d8fb315b136e16216385c
Hassan Rahman <hr-awesome@hotmail.com>
2023-05-25 01:02:21 -0500

    Add linux dotfiles configuration

---
+++ linux/laptop/emacs/.doom.d/custom.el
@@ -0,0 +1,15 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(auth-source-save-behavior nil)
+ '(ispell-dictionary nil)
+ '(package-selected-packages
+   '(treemacs-all-the-icons cider clojure-mode-extra-font-locking clojure-mode)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
